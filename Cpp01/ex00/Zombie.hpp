6e33a258f92f84c85e76d7e7a81beb8539c6c6e8
StefanPenev <stfnpenev@gmail.com>
2025-03-07 15:37:04 +0100

    fixes

--- Cpp01/ex00/Zombie.hpp
+++ Cpp01/ex00/Zombie.hpp
@@ -6,12 +6,12 @@
 /*   By: stefan <stefan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 20:29:57 by stefan            #+#    #+#             */
-/*   Updated: 2025/03/06 12:52:19 by stefan           ###   ########.fr       */
+/*   Updated: 2025/03/07 15:34:56 by stefan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_H
-# define ZOMBIE_H
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 #include <iostream>
 
--- Cpp01/ex01/Zombie.hpp
+++ Cpp01/ex01/Zombie.hpp
@@ -6,12 +6,12 @@
 /*   By: stefan <stefan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 13:06:11 by stefan            #+#    #+#             */
-/*   Updated: 2025/03/06 13:51:43 by stefan           ###   ########.fr       */
+/*   Updated: 2025/03/07 15:35:22 by stefan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_H
-# define ZOMBIE_H
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
 
 #include <iostream>
 
