2503986fcb009c1f47d01051e0a1a209b2182b8d
StefanPenev <stfnpenev@gmail.com>
2025-05-05 08:51:34 +0200

    cpp08/ex00

---
+++ Cpp08/ex00/easyfind.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stefan <stefan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 08:48:44 by stefan            #+#    #+#             */
+/*   Updated: 2025/05/05 08:49:13 by stefan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <exception>
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end()) {
+        throw std::runtime_error("Value not found in container");
+    }
+    return it;
+}
+
+#endif
---
+++ Cpp08/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stefan <stefan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 08:48:11 by stefan            #+#    #+#             */
+/*   Updated: 2025/05/05 08:51:10 by stefan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "easyfind.hpp"
+
+int main() {
+    try {
+        std::vector<int> vec;
+        vec.push_back(10);
+        vec.push_back(20);
+        vec.push_back(30);
+
+        std::vector<int>::iterator itVec = easyfind(vec, 20);
+        std::cout << "Found in vector: " << *itVec << std::endl;
+
+        std::list<int> lst;
+        lst.push_back(5);
+        lst.push_back(15);
+        lst.push_back(25);
+
+        std::list<int>::iterator itList = easyfind(lst, 100); // Will throw
+        std::cout << "Found in list: " << *itList << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
